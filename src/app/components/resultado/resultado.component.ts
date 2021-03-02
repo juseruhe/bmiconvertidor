0aa982c301b10ae774974458e578597254e9d6ec
Sebastian <jsruiz7411@misena.edu.co>
2021-03-02 16:48:12 -0500

    BMI Convertidor Terminado

--- src/app/components/resultado/resultado.component.ts
+++ src/app/components/resultado/resultado.component.ts
@@ -9,15 +9,45 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class ResultadoComponent implements OnInit {
 
   bmi: number;
+  resultado: string;
+  interpretacion: string;
 
 
   constructor(private route: ActivatedRoute) { 
+
+    this.resultado = '';
+    this.interpretacion='';
+
 
     this.bmi = +route.snapshot.paramMap.get('valor')!;
     console.log(this.bmi)
   }
 
   ngOnInit(): void {
+
+    this.getResultado();
+  }
+
+
+  getResultado(){
+
+    if(this.bmi >= 25){
+
+      this.resultado = "Exceso de Peso"
+      this.interpretacion = "Tienes un peso corporal superior al normal. Intente hacer ejercicio"
+}
+
+else if(this.bmi >= 18.5){
+
+  this.resultado = "Normal"
+  this.interpretacion = "Tienes un peso corporal normal. ¡Buen Trabajo!"
+
+}
+else{
+
+  this.resultado = "Bajo Peso"
+  this.interpretacion = "Tienes un peso corporal bajo al normal. Puedes comer un poco más"
+}
   }
 
 }
